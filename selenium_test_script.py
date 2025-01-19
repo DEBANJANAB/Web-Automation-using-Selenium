6398144f72087722d2e6f2b7dc2735d675b2689a
Debanjona Bhattacharjya <37831166+DEBANJANAB@users.noreply.github.com>
2025-01-19 20:51:28 +0100

    Update selenium_test_script.py

--- selenium_test_script.py
+++ selenium_test_script.py
@@ -4,13 +4,17 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 import time
 
-# Initialize WebDriver 
+
+url=input("Enter the url: ")
+username_input=input("Enter the username: ")
+password_input=input("Enter the password: ")
+ 
 driver_path = r"C:\Users\parit\Downloads\edgedriver_win64 (1)\msedgedriver.exe"  
 service = Service(driver_path)
 driver = webdriver.Edge(service=service)
     
-# Open the login page
-driver.get('http://the-internet.herokuapp.com/login')
+
+driver.get(url)
 
 # Find the username and password input fields and login button
 username = driver.find_element(By.ID, 'username')
@@ -18,8 +22,8 @@ password = driver.find_element(By.ID, 'password')
 login_button = driver.find_element(By.XPATH, '//button[@type="submit"]')
 
 # Perform login action
-username.send_keys('tomsmith')  # Replace with valid username for testing
-password.send_keys('SuperSecretPassword!')  # Replace with valid password
+username.send_keys(username_input)  # Replace with valid username for testing
+password.send_keys(password_input)  # Replace with valid password
 login_button.click()
 
 # Wait for the page to load after login (optional but sometimes necessary)
